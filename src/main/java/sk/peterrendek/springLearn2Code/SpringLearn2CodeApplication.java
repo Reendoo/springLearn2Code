53e68881b204138fedcebf5c5565048f06935681
Rendo <rendekml@peterrendek.sk>
2023-03-13 16:50:45 +0100

    first commit video-02

---
+++ src/main/java/sk/peterrendek/springLearn2Code/SpringLearn2CodeApplication.java
@@ -0,0 +1,13 @@
+package sk.peterrendek.springLearn2Code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearn2CodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearn2CodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/peterrendek/springLearn2Code/SpringLearn2CodeApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.peterrendek.springLearn2Code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearn2CodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
