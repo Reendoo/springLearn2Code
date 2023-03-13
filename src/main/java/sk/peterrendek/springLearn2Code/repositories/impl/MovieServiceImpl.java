85674a3c01b2f5185d90022e54f58f0144dca5f3
Rendo <rendekml@peterrendek.sk>
2023-03-13 17:28:37 +0100

    doplnil konstrutor pre injektnutie spring beanu video-07

--- src/main/java/sk/peterrendek/springLearn2Code/repositories/impl/MovieServiceImpl.java
+++ src/main/java/sk/peterrendek/springLearn2Code/repositories/impl/MovieServiceImpl.java
@@ -1,11 +1,18 @@
 package sk.peterrendek.springLearn2Code.repositories.impl;
 
+import org.springframework.stereotype.Service;
 import sk.peterrendek.springLearn2Code.model.Movie;
 import sk.peterrendek.springLearn2Code.repositories.MovieRepository;
 import sk.peterrendek.springLearn2Code.services.MovieService;
-
+@Service
 public class MovieServiceImpl implements MovieService {
-    MovieRepository movieRepository;
+
+    MovieRepository movieRepository;        //musi byt oznaceny ako Spring beam Injecia implemetacie
+
+    public MovieServiceImpl(MovieRepository movieRepository) { //dodanie implemetacie zo Springu
+        this.movieRepository = movieRepository;
+    }
+
     @Override
     public Movie createAddAddMovie() {
         Movie movie = new Movie();
