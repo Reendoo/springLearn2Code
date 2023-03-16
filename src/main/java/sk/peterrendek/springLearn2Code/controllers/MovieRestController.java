1662659d3ea1dd34187c7323097e51464c7e3fc7
Rendo <rendekml@peterrendek.sk>
2023-03-16 16:30:26 +0100

    hotovo

--- src/main/java/sk/peterrendek/springLearn2Code/controllers/MovieRestController.java
+++ src/main/java/sk/peterrendek/springLearn2Code/controllers/MovieRestController.java
@@ -1,51 +1,91 @@
 package sk.peterrendek.springLearn2Code.controllers;
 
+
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.*;
 import sk.peterrendek.springLearn2Code.model.dto.MovieDTO;
 import sk.peterrendek.springLearn2Code.services.impl.MovieServiceImpl;
+import org.springframework.hateoas.RepresentationModel;
+
 
 import java.util.List;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+
 @RestController
 @RequestMapping("/myapi")
-public class MovieRestController {
+public class MovieRestController extends EntityModel   {
     MovieServiceImpl movieService;
+    MovieResourceAssembler movieResourceAssembler;
 
-    public MovieRestController(MovieServiceImpl movieService) {
+    public MovieRestController(MovieServiceImpl movieService,MovieResourceAssembler movieResourceAssembler) {
         this.movieService = movieService;
+        this.movieResourceAssembler=movieResourceAssembler;
     }
 
     @GetMapping("/movies/{id}")
-    MovieDTO getMovieById(@PathVariable long id){
-        return movieService.getMovieById(id);
+    EntityModel<MovieDTO> getMovieById(@PathVariable long id) {
+        MovieDTO movieDTO = movieService.getMovieById(id);
+        return movieResourceAssembler.toModel(movieDTO);
+
+//        EntityModel<MovieDTO> resource = EntityModel.of(movieDTO);
+//        Link link = linkTo(methodOn(this.getClass()).getMovieById(id)).withSelfRel();
+//        Link lin2 = linkTo(methodOn(this.getClass()).getAllbyName("")).withRel("movies");
+//        resource.add(link);
+//        resource.add(lin2);
+//        return resource;
+
+
+//        return EntityModel.of(movieDTO,
+//                linkTo(methodOn(MovieRestController.class).getMovieById(id)).withSelfRel(),
+//                linkTo(methodOn(MovieRestController.class).getAllbyName("")).withRel("movies")
+//        );
+
+
+
+
+
+
+
+
+
+
+
+
     }
-//    @GetMapping("/movies")
-//    List<MovieDTO> getAll(){
-//        return movieService.getAllMovies();
-//    }
+
+
+
 
     @GetMapping("/movies")
-    List<MovieDTO> getAllbyName(@RequestParam(required = false) String name){
-        if(name!=null && !name.isEmpty()){
+    List<MovieDTO> getAllbyName(@RequestParam(required = false) String name) {
+        if (name != null && !name.isEmpty()) {
             return movieService.getMoviesByName(name);
-        }else{
+        } else {
             return movieService.getAllMovies();
         }
     }
+
     @PostMapping("/movies")
-    MovieDTO addMovie(@RequestBody MovieDTO movieDTO){
+    MovieDTO addMovie(@RequestBody MovieDTO movieDTO) {
         return movieService.addMovie(movieDTO);
     }
+
     @PutMapping("/movies/{id}")
-    MovieDTO updateMovie(@RequestBody MovieDTO movieDTO,@PathVariable long id){
-        return movieService.updateMovie(movieDTO,id);
+    EntityModel<MovieDTO> updateMovie(@RequestBody MovieDTO movieDTO, @PathVariable long id) {
+        MovieDTO d =movieService.updateMovie(movieDTO, id);
+        return movieResourceAssembler.toModel(d);
     }
 
     @DeleteMapping("/movies/{id}")
-    void deleteById(@PathVariable long id){
+    void deleteById(@PathVariable long id) {
         movieService.deleteById(id);
     }
-
 
 
 }
